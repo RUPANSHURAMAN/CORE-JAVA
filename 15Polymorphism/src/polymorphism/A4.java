b97f4737a5329f4ad2ba606591ca2962401843a0
root <root@Lenovo>
2019-11-01 21:57:14 +0530

    k

--- 15Polymorphism/src/polymorphism/A4.java
+++ 15Polymorphism/src/polymorphism/A4.java
@@ -1,5 +1,5 @@
 package polymorphism;
-//overloading: we create multiple methods with same name but then are differentiated on number and/or type of args, return type, applicable on static and non-static methods e.g. bolw
+//overloading: we create multiple methods with same name but then are differentiated on number and/or type of args, applicable on static and non-static methods e.g. bolw
 //polymorphism is run time and compile time, as overrrdiing happens when .class file runs whereas compile time happens [during] .class file is being created
 public class A4 {
 	private static float test(float f) {
